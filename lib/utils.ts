d216f8f39f2832b0183d4d875a1fdf98335c3b84
Yuta Yamaguchi <yamaguchi0607@gmail.com>
2025-06-04 03:15:10 +0900

    グッズの検索機能を実装

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
+
+// ひらがなをカタカナに変換
+export function toKatakana(str: string) {
+  return str.replace(/[\u3041-\u3096]/g, (char) => String.fromCharCode(char.charCodeAt(0) + 0x60));
+}
