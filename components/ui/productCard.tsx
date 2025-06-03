a38f9aadf1b760b1eab5a2e187fdb4d68cda886f
Yuta Yamaguchi <yamaguchi0607@gmail.com>
2025-06-04 04:35:13 +0900

    モバイル向けにスタイルを修正

--- components/ui/productCard.tsx
+++ components/ui/productCard.tsx
@@ -14,7 +14,7 @@ export function ProductCard({ name, price, imageUrl, storeUrl }: Props) {
   return (
     <div
       key={name}
-      className="flex flex-col justify-between rounded-3xl bg-white p-4 shadow-md transition-shadow hover:shadow-lg"
+      className="flex flex-col justify-between rounded-3xl bg-white p-2 shadow-md transition-shadow hover:shadow-lg md:p-4"
     >
       <div className="mb-4 overflow-hidden rounded-2xl">
         <Image
@@ -26,7 +26,7 @@ export function ProductCard({ name, price, imageUrl, storeUrl }: Props) {
           basePath={process.env.NEXT_PUBLIC_BASE_PATH}
         />
       </div>
-      <h3 className="mb-1 text-lg font-bold text-amber-700">{name}</h3>
+      <h3 className="mb-1 text-sm font-bold text-amber-700 md:text-base">{name}</h3>
       <div>
         <p className="mb-3 text-sm text-amber-500">{price}</p>
         <Link href={storeUrl!} target="_blank" rel="noopener noreferrer">
